18cfdb8804e70955984e9b28d0fe29bd435c9ca7
Tahsin0909 <tajisan789@gmail.com>
2023-11-27 19:38:55 +0600

    Added Loading state to Trainer PAge

--- src/Components/DasBoard PAge/AllTrainer/Alltrainer.jsx
+++ src/Components/DasBoard PAge/AllTrainer/Alltrainer.jsx
@@ -28,46 +28,79 @@ const AllTrainer = () => {
                         <tbody>
                             {/* row 1 */}
                             {
-                                data?.map(trainer => <tr key={trainer._id}>
+                                isPending ? (<>
                                     <td>
-                                        <div className="flex items-center gap-3">
-                                            <div className="avatar">
-                                                <div className="mask mask-squircle w-12 h-12">
-                                                    <img src={trainer.photo_url} alt="Avatar Tailwind CSS Component" />
+                                        <div className="flex gap-4 items-center">
+                                            <div className="skeleton w-16 h-16 rounded-full shrink-0"></div>
+                                            <div className="flex flex-col gap-4">
+                                                <div className="skeleton h-4 w-10"></div>
+                                                <div className="skeleton h-4 w-6"></div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div className="skeleton h-14 w-full"></div>
+                                    </td>
+                                    <td className="grid grid-cols-2 gap-2">
+                                        <div className="skeleton h-4 w-full"></div>
+                                        <div className="skeleton h-4 w-full"></div>
+                                        <div className="skeleton h-4 w-full"></div>
+                                        <div className="skeleton h-4 w-full"></div>
+                                    </td>
+                                    <td>
+                                        <div className="skeleton h-8"></div>
+                                    </td>
+                                    <td>
+                                        <div className="skeleton h-8 "></div>
+                                    </td>
+                                    <td>
+                                        <div className="skeleton h-8 "></div>
+                                    </td>
+                                </>
+                                )
+                                    :
+
+                                    data?.map(trainer => <tr key={trainer._id}>
+                                        <td>
+                                            <div className="flex items-center gap-3">
+                                                <div className="avatar">
+                                                    <div className="mask mask-squircle w-12 h-12">
+                                                        <img src={trainer.photo_url} alt="Avatar Tailwind CSS Component" />
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <div className="font-bold">{trainer.name}</div>
+                                                    <div className="text-sm opacity-50">Age: {trainer.age}</div>
                                                 </div>
                                             </div>
-                                            <div>
-                                                <div className="font-bold">{trainer.name}</div>
-                                                <div className="text-sm opacity-50">Age: {trainer.age}</div>
+                                        </td>
+                                        <td>
+                                            <p>{trainer.joinDate}</p>
+                                        </td>
+                                        <td>
+                                            <div className="grid grid-cols-2 gap-2">
+                                                {
+                                                    trainer.skill.slice(0, 4).map((data, idx) => <p className="text-xs bg-sky-100 rounded-md w-24 p-[2px] pl-[2px]" key={idx}>{data}</p>)
+                                                }
                                             </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p>{trainer.joinDate}</p>
-                                    </td>
-                                    <td>
-                                        <div className="grid grid-cols-2 gap-2">
-                                            {
-                                                trainer.skill.slice(0, 4).map((data, idx) => <p className="text-xs bg-sky-100 rounded-md w-24 p-[2px] pl-[2px]" key={idx}>{data}</p>)
+                                        </td>
+                                        <td>
+                                            <p>${trainer.salary}</p>
+                                        </td>
+                                        <td>
+                                            <p>{trainer.lastPayment == month ? 'paid' : 'pending'}</p>
+                                        </td>
+                                        <td>
+                                            {trainer.lastPayment != month &&
+                                                <div className=" w-[70px] h-[50px] flex justify-center items-center">
+                                                    <button>
+                                                        <Button text={'Pay'} />
+                                                    </button>
+                                                </div>
                                             }
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <p>${trainer.salary}</p>
-                                    </td>
-                                    <td>
-                                        <p>{trainer.lastPayment == month ? 'paid' : 'pending'}</p>
-                                    </td>
-                                    <td>
-                                        {trainer.lastPayment != month &&
-                                            <div className=" w-[70px] h-[50px] flex justify-center items-center">
-                                                <button>
-                                                    <Button text={'Pay'} />
-                                                </button>
-                                            </div>
-                                        }
-                                    </td>
-                                </tr>)
+                                        </td>
+                                    </tr>)
+
                             }
 
                         </tbody>

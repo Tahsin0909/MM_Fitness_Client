dd3a2820e5b4965aeb39691a0fa8d22a84897f12
Tahsin0909 <tajisan789@gmail.com>
2023-12-13 17:21:56 +0600

    Added BMI Feature

--- src/Components/MyActivity/Myactivity.jsx
+++ src/Components/MyActivity/Myactivity.jsx
@@ -4,13 +4,15 @@ import useAxiosPublic from "../../Hooks/useAxiosPublic";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Helmet } from "react-helmet";
+import { useForm } from "react-hook-form";
 
 const MyActivity = () => {
     const axiosPublic = useAxiosPublic()
     const { AuthUser } = useContext(AuthContext)
-    console.log(AuthUser);
+    const { register, handleSubmit, reset } = useForm();
     const [classData, setClassData] = useState(null)
     const [trainerData, setTrainerData] = useState(null)
+
     useEffect(() => {
         if (AuthUser) {
             axiosPublic.get(`/myClass/${AuthUser?.email}`)
@@ -19,6 +21,36 @@ const MyActivity = () => {
                 .then(res => setTrainerData(res.data))
         }
     }, [AuthUser])
+    // handle bmi
+    const [bmi, setBMI] = useState(null);
+    const [tips, setTips] = useState('Please Provide Us Your Weight in KG and Height In Meter So TAHt We can Guide You');
+    const [BmiColor, setBmiColor] = useState('');
+    const onSubmit = data => {
+        const weight = data.weight
+        const height = data.height
+        const heightSqr = parseFloat(height) * parseFloat(height)
+        const BMI = parseInt(weight) / heightSqr
+        setBMI(BMI.toFixed(2))
+        switch (true) {
+            case BMI < 18.5:
+              setTips('You are underweight. Consider increasing calorie intake.');
+              setBmiColor("blue")
+              break;
+            case BMI >= 18.5 && BMI <= 24.9:
+              setTips('You are in the normal weight range. Maintain a balanced diet and exercise regularly.');
+              setBmiColor("green")
+              break;
+            case BMI >= 25 && BMI <= 29.9:
+              setTips('You are overweight. Focus on portion control and exercise regularly.');
+              setBmiColor("orange")
+              break;
+            default:
+              setTips('You are in the obese range. Consider consulting a healthcare professional for guidance.');
+              setBmiColor("red")
+              break;
+          }
+          reset()
+    }
 
     return (
         <div>
@@ -26,6 +58,7 @@ const MyActivity = () => {
                 <title>Mm Dashboard | My Activity</title>
             </Helmet>
             <div className="flex  gap-6 mt-10">
+                {/* class JOined */}
                 <div className="border w-[500px] p-4 rounded-md shadow-lg">
                     <p className="text-lg font-semibold ">Total Class: {classData?.length}</p>
                     <div className="overflow-x-auto ">
@@ -65,6 +98,7 @@ const MyActivity = () => {
                         </table>
                     </div>
                 </div>
+                {/* booked trainer */}
                 <div className="border w-[500px] p-4 rounded-md shadow-lg">
                     <p className="text-lg font-semibold ">Trainer Booked: {trainerData?.length}</p>
                     <div className="overflow-x-auto ">
@@ -93,6 +127,38 @@ const MyActivity = () => {
                             </tbody>
                         </table>
                     </div>
+                </div>
+            </div>
+            {/* Calculate Your BMI */}
+            <div className="mt-6 border w-[700px] p-4 rounded-md shadow-lg flex gap-10">
+                <div>
+                    <p className="text-lg font-semibold ">Calculate Your BMI:</p>
+                    <form onSubmit={handleSubmit(onSubmit)} className="">
+                        <div className="flex gap-4">
+                            <div className="">
+                                <label className="label">
+                                    <span className="label-text text-black">Weight</span>
+                                </label>
+
+                                <input {...register("weight")} type="number" placeholder="kg" className="input input-bordered w-[100px] h-[40px] " required />
+
+                            </div>
+                            <div className="">
+                                <label className="label">
+                                    <span className="label-text text-black">Height</span>
+                                </label>
+
+                                <input {...register("height")} type="text" placeholder="meter" className="input input-bordered w-[100px] h-[40px] " required />
+                            </div>
+                        </div>
+                        <div className="mt-2">
+                            <input className="btn Shared_Color w-[216px] h-[40px]" type="submit" value="Calculate" />
+                        </div>
+                    </form>
+                </div>
+                <div className="">
+                    <p style={{ color: `${BmiColor}` }} className="text-lg font-semibold">Your BMI: {bmi}</p>
+                    <p style={{ color: `${BmiColor}` }}  className="mt-6 text-lg">{tips}</p>
                 </div>
             </div>
         </div>
